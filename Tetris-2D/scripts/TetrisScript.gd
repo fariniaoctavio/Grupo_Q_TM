2aac6d404dee269e6f5c5ae48bb06017f68cfadb
fariniaoctavio <octavio.farina@alumnos.frm.utn.edu.ar>
2022-06-01 01:17:40 -0300

    pausar e iniciar musica

--- Tetris-2D/scripts/TetrisScript.gd
+++ Tetris-2D/scripts/TetrisScript.gd
@@ -1,1 +1,41 @@
-extends Node
+extends CenterContainer
+
+enum { DETENIDO, INICIADO, PAUSADO, DETENER, INICIAR, PAUSAR}
+
+var interfaz
+var posicion_cancion = 0
+
+func _ready():
+	interfaz = $Interfaz
+	interfaz.connect("button_pressed", self, "_button_pressed")
+	
+func _button_pressed(nombre_boton):
+	match nombre_boton:
+		"Nuevo Juego":
+			print("JUEGO NUEVO TOCADO")
+		"Pausa":
+			print("PAUSA TOCADO")
+		"Salir":
+			print("SALIR TOCADO")
+		"ApagarEncenderMusica":
+			print("MUSICA TOCADO")
+			if !_musica_is_on():
+				_musica(INICIAR)
+			else:
+				_musica(PAUSAR)
+
+func _musica(accion):
+	if accion == INICIAR:
+		$ReproductorMusica.volume_db = -18
+		if !$ReproductorMusica.is_playing():
+			$ReproductorMusica.play(posicion_cancion)
+		print("Musica Iniciada")
+	else:
+		posicion_cancion = $ReproductorMusica.get_playback_position()
+		$ReproductorMusica.volume_db = 0
+		$ReproductorMusica.stop()
+		print("Musica Pausada")
+
+
+func _musica_is_on():
+	return $ReproductorMusica.volume_db == -18
