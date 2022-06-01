b2fbaded339205b55357b9da5b6e4e1aca553631
Franco Minati <franco.minati@alumnos.frm.utn.edu.ar>
2022-06-01 01:11:59 -0300

    formas script

--- Tetris-2D/scripts/FormasScript.gd
+++ Tetris-2D/scripts/FormasScript.gd
@@ -1,16 +1,29 @@
-extends Node
+extends Control
 
+class_name FormasInformacion
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var color: Color
+var grilla: Array
+var coordenadas: Array
+var celdas:GridContainer
 
+	#No se puede rotar un grid, pero como su padre 
+	#es un objeto 2d entonces es posible rotarlo
+func rotar_izq():
+	celdas.get_parent().rotate(-PI/2)
+	_rotar_grilla(1,-1)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func rotar_der():
+	celdas.get_parent().rotate(PI/2)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _rotar_grilla(sign_of_x,sing_of_y):
+	var grilla_rotada=grilla.duplicate(true)
+	for x in coordenadas:
+		for y in coordenadas:
+			#Trasformo x e y en indices de una matriz de rotacion
+			var x1=coordenadas.find(x)
+			var y1=coordenadas.find(y)
+			var x2=coordenadas.find(sing_of_y*y)
+			var y2=coordenadas.find(sign_of_x*x)
+			grilla_rotada[y1][x1]=grilla[y2][x2]
+	grilla=grilla_rotada
