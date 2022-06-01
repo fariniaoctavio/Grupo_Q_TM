7797ac42e2d58c044ec080bf222c4ed38f3f39f2
Franco Minati <franco.minati@alumnos.frm.utn.edu.ar>
2022-05-31 23:56:30 -0300

    GUI y avance script

--- Tetris-2D/scripts/InterfazScript.gd
+++ Tetris-2D/scripts/InterfazScript.gd
@@ -1,16 +1,26 @@
 extends Node
 
+var grilla
+var SIGUIENTE
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
+const CELDA_FONDO1=Color(1,1,1)
+const CELDA_FONDO2=Color(0)
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+#busca el nodo correspondiente
+	grilla = find_node("Grilla")
+	SIGUIENTE=find_node("SiguienteForma")
+	add_cells(grilla,200)
+	clear_cells(grilla,CELDA_FONDO1)
+	clear_cells(SIGUIENTE,CELDA_FONDO2)
+func add_cells(node,n):
+	var numero_de_celdas=node.get_child_count()
+	#Mientras que el numero de celdas sea menor que n (numero de celdas de la escena)
+	#Se agregan nuevas celdas, es decir hasta 200
+	while numero_de_celdas < n:
+		node.add_child(node.get_child(0).duplicate())
+		numero_de_celdas+=1
+#esta funcion le da el color a las celdas
+func clear_cells(node,color):
+	for cell in node.get_children():
+		cell.modulate=color
+		
